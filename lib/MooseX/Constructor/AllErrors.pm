ab06be02f48e86f7b15945e8f6233d07b07c6e99
Jesse Luehrs <doy@tozt.net>
2009-12-20 12:16:08 -0600

    use new Moose::Exporter features

--- lib/MooseX/Constructor/AllErrors.pm
+++ lib/MooseX/Constructor/AllErrors.pm
@@ -1,34 +1,14 @@
 package MooseX::Constructor::AllErrors;
 
-use strict;
-use warnings;
-
 use Moose ();
 use Moose::Exporter;
-use Moose::Util::MetaRole;
-use MooseX::Constructor::AllErrors::Role::Object;
-use MooseX::Constructor::AllErrors::Role::Meta::Method::Constructor;
 
-Moose::Exporter->setup_import_methods;
-
-sub init_meta {
-  shift;
-  my %options = @_;
-
-  Moose->init_meta(%options);
-
-  Moose::Util::MetaRole::apply_base_class_roles(
-    for_class => $options{for_class},
-    roles => [ 'MooseX::Constructor::AllErrors::Role::Object' ],
-  );
-
-  Moose::Util::MetaRole::apply_metaclass_roles(
-    for_class => $options{for_class},
+Moose::Exporter->setup_import_methods(
+    base_class_roles => [ 'MooseX::Constructor::AllErrors::Role::Object' ],
     constructor_class_roles => [
       'MooseX::Constructor::AllErrors::Role::Meta::Method::Constructor',
     ],
-  );
-}
+);
 
 1;
 
@@ -59,12 +39,6 @@ the construction of your objects, rather than halting after the first.
 If there are errors, C<$@> will contain a
 L<MooseX::Constructor::AllErrors::Error::Constructor> object.  See its
 documentation for possible error types.
-
-=begin Pod::Coverage
-
-init_meta
-
-=end Pod::Coverage
 
 =head1 SEE ALSO
 
